bf568be042af7a0e3bc484b8a0717236349f5473
egabriel-castillom <egabriel.castillom@gmail.com>
2023-06-16 23:01:31 -0600

    NoInterrupcion

--- static/menuDesliz.js
+++ static/menuDesliz.js
@@ -3,10 +3,17 @@ if (event.key === 'ArrowUp' || event.key === 'ArrowDown') {
     cambiar(event.key);
 }
 });
+
 //Inicializar variable para controlar el botón actual.
 let idBoton = ['#iniciar','#memorama','#terminar'];
 let idBValor = 0;
+let llamada = 0;
+sessionStorage.setItem('llamada0',JSON.stringify(llamada))
+sessionStorage.setItem('llamada1',JSON.stringify(llamada))
+sessionStorage.setItem('llamada2',JSON.stringify(llamada))
 
+//Funcion inicial para ejecutar secuencia a lo largo de 
+//el texto contenedor del botón
 function cambiar(direccion) {
 if (direccion ==='ArrowUp'){
     idBValor = idBValor - 1;
@@ -20,28 +27,18 @@ if (idBValor > 2){
     idBValor = 0;
 };
 
-let botonActivo = idBoton[idBValor]
-
-const btn = document.querySelector(botonActivo);
-let nuevoContenido = btn.textContent;
-
-// Función auxiliar para mostrar la secuencia de caracteres
-function mostrarSecuencia(i) {
-    if (i < nuevoContenido.length) {
-    let j = 0;
-    let secuencia = [">", "*", "<", "*"];
-    let intervalo = setInterval(() => {
-        btn.textContent = nuevoContenido.slice(0, i) + secuencia[j];
-        j++;
-        if (j === secuencia.length) {
-        clearInterval(intervalo);
-        btn.textContent = nuevoContenido.slice(0, i + 1);
-        mostrarSecuencia(i + 1);
-        }
-    }, 40);
-    }
+//Se comprueba si es que el boton seleccionado se encuentra ejecutando
+//una secuencia, de ser así no se permite interrumpirla
+console.log(idBValor.toString());
+if (JSON.parse(sessionStorage.getItem('llamada'+idBValor.toString())) !== 1){
+    let botonActivo = idBoton[idBValor]
+    const btn = document.querySelector(botonActivo);
+    let nuevoContenido = btn.textContent;
+    
+    //Llamar a la función auxiliar para el primer carácter
+    if (JSON.parse(sessionStorage.getItem('llamada0')) === 0 || JSON.parse(sessionStorage.getItem('llamada1')) === 0 || JSON.parse(sessionStorage.getItem('llamada2')) === 0){
+        mostrarSecuencia(0,btn,nuevoContenido,idBValor);
+    };
 }
 
-// Llamar a la función auxiliar para el primer carácter
-mostrarSecuencia(0);
 };
---
+++ static/secuenciaPalabra.js
@@ -0,0 +1,37 @@
+// Función auxiliar para mostrar la secuencia de caracteres
+function mostrarSecuencia(i,btn,nuevoContenido,idBValor) {
+    let llamada = 1;
+    if (idBValor === 0){
+        sessionStorage.setItem('llamada0',JSON.stringify(llamada));
+    };
+    if (idBValor === 1){
+        sessionStorage.setItem('llamada1',JSON.stringify(llamada));
+    };
+    if (idBValor === 2){
+        sessionStorage.setItem('llamada2',JSON.stringify(llamada));
+    };
+    if (i < nuevoContenido.length) {
+    let j = 0;
+    let secuencia = [">", "*", "<", "*"];
+    let intervalo = setInterval(() => {
+        btn.textContent = nuevoContenido.slice(0, i) + secuencia[j];
+        j++;
+        if (j === secuencia.length) {
+        clearInterval(intervalo);
+        btn.textContent = nuevoContenido.slice(0, i + 1);
+        mostrarSecuencia(i + 1,btn,nuevoContenido,idBValor);
+        }
+    }, 40);
+    }else{
+        llamada = 0;
+        if (idBValor === 0){
+            sessionStorage.setItem('llamada0',JSON.stringify(llamada));
+        };
+        if (idBValor === 1){
+            sessionStorage.setItem('llamada1',JSON.stringify(llamada));
+        };
+        if (idBValor === 2){
+            sessionStorage.setItem('llamada2',JSON.stringify(llamada));
+        };
+    }
+};
